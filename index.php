25cc9847a9242c986afb20672e0730e9bd2283f4
Marina-Dev <offrinmarina343@gmail.com>
2020-09-22 14:39:12 +0100

    - sign up form

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+   
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+
+    <div>
+    
+    <form style="margin:0%;padding-left: 500px;padding-top: 50px;">
+        <h1 style="text-decoration: overline;">Sign-up</h1>
+        <input type="text" name="user" id="user" placeholder="Username" required="true"> <br><br>
+        <input type="email" name="mail" id="mail" placeholder="E-mail" required="true"><br><br>
+        <input type="password" name="pass1" id="pass1" placeholder="password" required="true"><br><br>
+        <input type="password" name="pass2" id="pass2" placeholder="confirm password" required="true"><br><br>
+        <input type="submit" value="Sign up">
+    </form>
+</div>
+
+
+</head>
+<body>
+    
+</body>
+</html>
